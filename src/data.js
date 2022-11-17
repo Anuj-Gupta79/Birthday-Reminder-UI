9a6ca9b9812630e89726cf45c521b661d2ce5b05
anuj <anujgupta7906@gmail.com>
2022-11-17 23:34:08 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id:1,
+        name:'Mahima Sharma',
+        age:'20th',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjtijpuBsVPMwJl3lplVhKGdiVnoJwhbvlSOxpPNI8WGT3YOTlCVabwjWSljBwdxpFG1s&usqp=CAU',
+    },
+    {
+        id:2,
+        name:'Raman Sharma',
+        age:'21st',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTim3xEAZOhgri-8RZ5ORly_E4x3h0GNu3n6Y9-oIva&s',
+    },
+    {
+        id:3,
+        name:'Garima Agarwal',
+        age:'22nd',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS_yE2RJMtGZMTVxp4ovcL279BnDWG9aKS8oxQRQY&s',
+    },
+    {
+        id:4,
+        name:'Akash Agarwal',
+        age:'22nd',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbY4qPjMTTgRUsZEXzmVD4M09spEzqsw05EuKhhE6c_yHcgv75hHD2O7PKm1unM42fKnk&usqp=CAU',
+    },
+    {
+        id:5,
+        name:'Ranjeev Thakur',
+        age:'19th',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmTuB9V3hmeOzEVGWVPTtDvB8tyYgz9sl7-G58c-RWbydL9vmiznXUSDtobRw2gwOUfkQ&usqp=CAU',
+    },
+    {
+        id:6,
+        name:'Khushi Thakur',
+        age:'20th',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuRZkxDTYE2B85z8Zq5U4rsPxCfQ8JAjN4SzFMQ6RiEYErXxIMsDNCL4HYgkAaiuvdalM&usqp=CAU',
+    },
+]
